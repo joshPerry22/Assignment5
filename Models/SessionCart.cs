b33589d88966046d7c2ee973807c686555e088b5
joshPerry22 <jsp1955@byu.edu>
2021-03-09 16:10:18 -0700

    TA help

--- Models/SessionCart.cs
+++ Models/SessionCart.cs
@@ -1,39 +1,39 @@
-﻿//using System;
-//using System.Text.Json.Serialization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.Extensions.DependencyInjection;
-//using Assignment5.Infrastructure;
-//using Assignment5.Models;
+﻿using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Assignment5.Infrastructure;
+using Assignment5.Models;
 
-//namespace SportsStore.Models
-//{
-//    public class SessionCart : Cart
-//    {
-//        public static Cart GetCart(IServiceProvider services)
-//        {
-//            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
-//                .HttpContext.Session;
-//            SessionCart cart = session?.GetJson<SessionCart>("Cart")
-//                ?? new SessionCart();
-//            cart.Session = session;
-//            return cart;
-//        }
-//        [JsonIgnore]
-//        public ISession Session { get; set; }
-//        public override void AddItem(Project proj, int quantity)
-//        {
-//            base.AddItem(proj, quantity);
-//            Session.SetJson("Cart", this);
-//        }
-//        public override void RemoveLine(Project proj)
-//        {
-//            base.RemoveLine(proj);
-//            Session.SetJson("Cart", this);
-//        }
-//        public override void Clear()
-//        {
-//            base.Clear();
-//            Session.Remove("Cart");
-//        }
-//    }
-//}
+namespace SportsStore.Models
+{
+    public class SessionCart : Cart
+    {
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+            SessionCart cart = session?.GetJson<SessionCart>("Cart")
+                ?? new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+        [JsonIgnore]
+        public ISession Session { get; set; }
+        public override void AddItem(Project proj, int quantity)
+        {
+            base.AddItem(proj, quantity);
+            Session.SetJson("Cart", this);
+        }
+        public override void RemoveLine(Project proj)
+        {
+            base.RemoveLine(proj);
+            Session.SetJson("Cart", this);
+        }
+        public override void Clear()
+        {
+            base.Clear();
+            Session.Remove("Cart");
+        }
+    }
+}
